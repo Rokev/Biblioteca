43a9a08e9bc578e69c35ecf5b7b3698287595d54
Kev <kevin277507@gmail.com>
2026-03-02 16:34:21 -0500

    Clase Cliente creada, con constructor, getters y setters

---
+++ Cliente.java
@@ -0,0 +1,56 @@
+package Biblioteca;
+public class Cliente {
+    private int id;
+    private String nombre;
+    private String telefono;
+    private String dirección;
+
+
+    public Cliente(int id, String nombre, String telefono, String dirección) {
+        this.id = id;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.dirección = dirección;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
+    public String getDirección() {
+        return dirección;
+    }
+
+
+    public void setDirección(String dirección) {
+        this.dirección = dirección;
+    }
+    
+}
