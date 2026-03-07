7c691886ade27fa93ba9abcdb19806c75bffff1f
nataly <n4taly.morales24040@gmail.com>
2026-03-06 20:35:02 -0500

    cambios en el menu

--- Main.java
+++ Main.java
@@ -5,7 +5,8 @@ import Biblioteca.InventarioLibro;
 
 public class Main {
     public static void main(String[] args) {
-        InventarioLibro inventario = new InventarioLibro();
+        InventarioLibro inventario = new InventarioLibro();5
+        
         Scanner scanner = new Scanner(System.in);
         int opcion =0;
         while(opcion != 7){
@@ -47,7 +48,27 @@ public class Main {
                     // Lógica para agregar libro a cliente
                     break;
                     case 4:
-                inventario.registrarNuevoLibro();
+                System.out.println("Ingrese datos del libro a registrar en el inventario:");
+                    System.out.print("Identificador: ");
+                    int idNuevoLib = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Título: ");
+                    String t = scanner.nextLine();
+                    System.out.print("Autor: ");
+                    String a = scanner.nextLine();
+                    System.out.print("Editorial: ");
+                    String e = scanner.nextLine();
+                    System.out.print("Año publicación: ");
+                    int y = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.print("Categoría: ");
+                    String c = scanner.nextLine();
+                    Libro lNuevo = new Libro(idNuevoLib, t, a, e, y, c, "disponible");
+                    if (inventario.registrarNuevoLibro(lNuevo)) {
+                        System.out.println("Libro agregado al inventario.");
+                    } else {
+                        System.out.println("Ya existe un libro con ese identificador.");
+                    }
                     break;
                 case 5:
                 inventario.listarLibros();
@@ -58,13 +79,4 @@ public class Main {
         }
         scanner.close();
     }
-
-
-
-
-
-
-
-
-
 }
